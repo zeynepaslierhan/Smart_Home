f9fcfd5198a667c2dd2046e4ebac73269132e5b6
Asli <200202047@kocaeli.edu.tr>
2022-04-12 16:53:58 +0300

    dijital termometre
    arduıno bitti
    NOT:
    sıcaklık sabit gösteriyor.

--- ArduinoMega/ArduinoMega.ino
+++ ArduinoMega/ArduinoMega.ino
@@ -1,28 +1,57 @@
+//hareket sensörü için değişkenler
 int     sensor      = 8;
 boolean sensorValue = 0;
 int     tetik       = 7;
 
+//dijital termometre için değişkenler
+const int lm35 = A0;
+
+float voltaj_deger = 0;
+float sicaklik = 0;
+int gelen_veri = 0;
+
+#include <LiquidCrystal.h> //LCD için fonksiyon kütüphanesi
+LiquidCrystal lcd(6,9,5,4,3,2);//LCD üzerinde kullanılanlarla nesne oluşturuldu(sırayla rs,e..)
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);//seri haberleşmeyi başlatır.
+  
+  //hareket sensörü kurulumları
   pinMode(sensor,INPUT);
   pinMode(tetik,OUTPUT);
-  Serial.println("Started...");
+  Serial.println("Hareket sensörü çalışıyor..");
   delay(10);
+
+  //dijital termometre kurulumları
+  lcd.begin(16, 2);//16 karakterlik 2 satır LCD kullanılıyor. 
+  pinMode(lm35, INPUT);
 
 }
 
 void loop() {
+  
+  //hareket sensörü için döngüler
   sensorValue = digitalRead(sensor);
   Serial.println("Sensor Value="+String(sensorValue));
   delay(100);
 
   if (sensorValue == 1) {
     digitalWrite(tetik, HIGH);
-
-  } else {
+  }else{
     digitalWrite(tetik, LOW);
   }
+
+  //dijital termometre için döngüler
+  gelen_veri = analogRead(lm35);
+  voltaj_deger = (gelen_veri / 1023.0) * 5000;
+  sicaklik = voltaj_deger / 10.0;
+  lcd.clear();
+  lcd.home();
+  lcd.print("sicaklik = ");
+  lcd.setCursor(0, 1);
+  lcd.print(sicaklik);
+  lcd.print(" derece");
+  delay(1000);
 
 
 
